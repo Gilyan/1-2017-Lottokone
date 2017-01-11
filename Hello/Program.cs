10bca4edfddb7ce169a2de3bb5da5f8f6d265148
k8517 <k8517@ICT-421-WS-05>
2017-01-11 12:58:10 +0200

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,35 @@
+﻿/* **************************************
+Hello.cs
+
+Luotu 9.1.2017
+
+Toiminta: Tulostaa näytölle tekstiä eri tavoin. Harjoiteltu olion luontia.
+
+Minttu Mäkäläinen K8517
+************************************** */
+
+using System;                               // Sovellus käyttää System nimiavaruutta, sisältää C#:n perusluokkia - LAITA AAKKOSJÄRJESTYKSEEN
+
+namespace Hello                             // Määrittelee tämän sovelluksen nimiavaruuden
+{
+    class Henkilo                           // Luokka = Henkilo
+    {
+        public string Nimi { get; set; }    // Ominaisuus = Nimi. Get = voidaan lukea. Set = voidaan asettaa.
+        public int Ika { get; set; }        // Ominaisuus = Ika.
+    }
+    class Testi                             // Määrittelee sovelluksen pääluokan. Pitää olla yksikkömuodossa oleva substantiivi.
+    {
+        static void Main(string[] args)     // Näitä saa olla vain yksi!
+        {
+            string nimi = "Robert Jordan";          // Määritellään muuttuja
+            Console.WriteLine("Terve " + nimi);     // Tulostetaan näytölle muuttujan sisältö.
+
+            Henkilo hlo = new Henkilo();            // Luodaan olio Henkilo-luokasta
+            hlo.Nimi = "Terry Pratchett";
+            hlo.Ika = 66;
+            Console.WriteLine("Terve {0}, ikäsi on {1}.", hlo.Nimi, hlo.Ika); // Tulostetaan näytölle olion hlo ominaisuuksia. Käytetty kapselointia.
+
+            Console.ReadLine();                     // Pidetään sovelluksen ajoikkuna auki. Sulkeutuu enteristä. Ei välttämättä ole tarpeen.
+        }
+    }
+}
---
+++ Taulukkoharjoitus/Program.cs
@@ -0,0 +1,50 @@
+﻿/* **************************************
+Taulukkoharjoitus.cs
+
+Luotu 9.1.2017
+Muokattu 10.1.2017
+
+Toiminta: Kysyy käyttäjältä viisi käyttäjänimeä ja tallentaa ne taulukkoon.
+Tulostetaan nimet ruudulle aakkosjärjestyksessä.
+
+Minttu Mäkäläinen K8517
+************************************** */
+
+using System;                               // Sovellus käyttää System nimiavaruutta, sisältää C#:n perusluokkia - LAITA AAKKOSJÄRJESTYKSEEN
+
+namespace Hello                             // Määrittelee tämän sovelluksen nimiavaruuden
+{
+    class Testi                             // Määrittelee sovelluksen pääluokan. Pitää olla yksikkömuodossa oleva substantiivi.
+    {
+        static void Main(string[] args)     // Näitä saa olla vain yksi!
+        {
+            string[] nimet = new string[5];         // Merkkijonotaulukko esitellään ja alustetaan
+
+            for (int i = 0; i < nimet.Length; i++)
+            {
+                Console.WriteLine("Anna nimi: ");
+                nimet[i] = Console.ReadLine();
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Annetut nimet olivat: ");
+            for (int i = 0; i < nimet.Length; i++)
+            {
+                Console.Write(nimet[i] + " ");
+            }
+
+            Array.Sort(nimet);
+
+            Console.WriteLine("\n\nAnnetut nimet aakkosjärjestyksessä:");
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write(nimet[i] + " ");
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
+
